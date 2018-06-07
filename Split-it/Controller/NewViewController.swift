c6690e897cf66d190dbc9d9ede02124ec677a6b9
Wilson (Zheng) Zhang <abovesummitwz@gmail.com>
2018-06-07 14:37:21 -0400

    Added 4 more ViewController and changed the navigation bar to the large title design.

---
+++ Split-it/Controller/NewViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NewViewController.swift
+//  Split-it
+//
+//  Created by Wilson Zhang on 6/7/18.
+//  Copyright © 2018 Zheng Zhang. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
